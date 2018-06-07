340fe89f6f40e9ae4eeff124802f674394f2c4b4
Erandir Junior <aefs12junior@gmail.com>
2018-06-07 16:31:57 -0300

    Praticando Test-Driven Development (TDD) continuação

--- Leilao.php
+++ Leilao.php
@@ -13,8 +13,24 @@ class Leilao
 
 	public function propoe(Lance $lance)
 	{
-		if (count($this->lances) === 0 || $this->pegaUltimoLance()->getUsuario() !== $lance->getUsuario())
+		if (count($this->lances) === 0 || $this->podeDarLance($lance->getUsuario()))
 			$this->lances[] = $lance;
+	}
+
+	private function podeDarLance(Usuario $usuario)
+	{
+		$total = $this->contaLancesDo($usuario);
+		return $this->pegaUltimoLance()->getUsuario() !== $usuario && $total < 5;
+	}
+
+	private function contaLancesDo(Usuario $usuario)
+	{
+		$total = 0;
+		foreach ($this->lances as $lanceAtual) {
+			if($lanceAtual->getUsuario() === $usuario) $total++;
+		}
+
+		return $total;
 	}
 
 	public function pegaUltimoLance()
--- LeilaoTest.php
+++ LeilaoTest.php
@@ -34,4 +34,33 @@ class LeilaoTeste extends PHPUnit_Framework_TestCase
 		$this->assertEquals(1, count($leilao->getLances()));
 		$this->assertEquals(2000, $leilao->getLances()[0]->getValor());
 	}
+
+	public function testDeveDarNoMaximoCincoLances()
+	{
+		$leilao = new Leilao('MacBook caro');
+
+		$jobs = new Usuario('Jobs');
+		$gates = new Usuario('Gates');
+
+		$leilao->propoe(new Lance($jobs, 2000));
+		$leilao->propoe(new Lance($gates, 3000));
+
+		$leilao->propoe(new Lance($jobs, 4000));
+		$leilao->propoe(new Lance($gates, 5000));
+
+		$leilao->propoe(new Lance($jobs, 6000));
+		$leilao->propoe(new Lance($gates, 7000));
+
+		$leilao->propoe(new Lance($jobs, 8000));
+		$leilao->propoe(new Lance($gates, 9000));
+
+		$leilao->propoe(new Lance($jobs, 10000));
+		$leilao->propoe(new Lance($gates, 11000));
+
+		// deve ser ignorado
+		$leilao->propoe(new Lance($jobs, 12000));
+
+		$this->assertEquals(10, count($leilao->getLances()));
+		$this->assertEquals(11000, $leilao->getLances()[9]->getValor());
+	}
 }
